2755ae1b45f673c6014be4fac2a10415d0b04062
PeterQuinn925 <peterquinn925@gmail.com>
2022-02-21 09:48:47 -0800

    Add files via upload

--- mqtt_subscribe.py
+++ mqtt_subscribe.py
@@ -3,18 +3,39 @@ import datetime
 import time
 import paho.mqtt.client as mqttClient
 import json
+from influxdb import InfluxDBClient
+import os
+
+INFLUXDB_ADDRESS = '127.0.0.1'
+INFLUXDB_USER = 'mqtt'
+INFLUXDB_PASSWORD = 'mqtt'
+INFLUXDB_DATABASE = 'weather'
 
 def on_message(client, userdata, message):
-    print("message received " ,str(message.payload.decode("utf-8")))
-    print("message topic=",message.topic)
+#    print("message received " ,str(message.payload.decode("utf-8")))
+#    print("message topic=",message.topic)
 #    print("message qos=",message.qos)
 #    print("message retain flag=",message.retain)
+    mqtt_msg = json.loads(message.payload)
+    timestamp = mqtt_msg["dateTime"]
+    del mqtt_msg['dateTime']
+    tags = {'foo':'none'}
+#    influx_msg = [{'fields':mqtt_msg, 'name':'observation', 'tags':tags,'timestamp':timestamp}]
+    influx_msg = [{'measurement': 'weather','fields':mqtt_msg, 'name':'observation', 'tags':tags,'timestamp':timestamp}]
+    influx_json = json.dumps(influx_msg)
+    print (influx_json)
+    influxdb_client.write_points(influx_msg)
+
+influxdb_client = InfluxDBClient(INFLUXDB_ADDRESS, 8086, INFLUXDB_USER, INFLUXDB_PASSWORD, None)
+influxdb_client.switch_database(INFLUXDB_DATABASE)
+
 
 Connected = False
 broker_address= "localhost"
 port = 1883
 topic = "weather/data"
-client = mqttClient.Client("Raspi4")
+clientname= "Raspi4_" + str(os.getpid()) #to make sure it's unique when it restarts
+client = mqttClient.Client(clientname)
 client.on_message=on_message
 client.connect(broker_address,port=port)
 #client.loop_forever()
