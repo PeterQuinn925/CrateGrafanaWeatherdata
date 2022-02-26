f1ff1a425127a00fccdbd14b4f1b926f245ea892
PeterQuinn925 <peterquinn925@gmail.com>
2022-02-26 14:00:44 -0800

    fix bad extraTemp2 values, correct for time zone

--- MoveHistory2Influxdb.py
+++ MoveHistory2Influxdb.py
@@ -18,9 +18,11 @@ conn = sqlite3.connect(database)
 #querystring = "SELECT dateTime, barometer,inTemp,extraTemp2,outTemp,pm2_5,pm10_0 from archive where dateTime>1644172560"
 querystring = "SELECT dateTime, barometer,inTemp,extraTemp2,outTemp,pm2_5,pm10_0 from archive where dateTime>?"
 #2 years
-lasttime = 1387200000
+#lasttime = 1387200000
 #1 month
 #lasttime = 1642903619
+#forever
+lasttime = 1000000000
 
 influxdb_client = InfluxDBClient(INFLUXDB_ADDRESS, 8086, INFLUXDB_USER, INFLUXDB_PASSWORD, None)
 influxdb_client.switch_database(INFLUXDB_DATABASE)
@@ -35,9 +37,13 @@ i=0
 for row in rows:
    payload = row
    timestamp = payload["dateTime"]
-   u_time = datetime.datetime.fromtimestamp(int(timestamp))
+#8 hours behind due to time zones, I think
+#8hr*60min*60sec
+   u_time = datetime.datetime.fromtimestamp(int(timestamp)+28800)
    f_time =u_time.strftime('%Y-%m-%dT%H:%M:%S') 
    del payload['dateTime']
+   if payload['extraTemp2'] is not None and payload['extraTemp2']>100:
+      payload['extraTemp2']=70
    tags = {'foo':'none'}
    influx_msg = [{'measurement': 'weather','fields':payload, 'name':'observation', 'tags':tags,'time':f_time}]
 #      print (influx_msg)
